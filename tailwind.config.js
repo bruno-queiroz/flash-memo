9dc196b254363b493e3d7b81389df920c4e811c1
bruno <brunoqueiroz612@gmail.com>
2023-04-28 15:23:04 -0300

    Extend tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-blue": "#1b4965",
+        "primary-yellow": "#ff9f1c",
+        "aqua-blue": "#2ec4b6",
+      },
+    },
   },
   plugins: [],
 };
