ee68576e6eca6109ff8427f72fd9b6c202b8a497
bruno <brunoqueiroz612@gmail.com>
2023-04-28 16:01:39 -0300

    Add dark theme to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,10 @@ const Header = () => {
   return (
     <header className="p-4">
       <div>
-        <Link to="/" className="text-dark-blue text-2xl font-bold">
+        <Link
+          to="/"
+          className="text-dark-blue dark:text-aqua-blue text-2xl font-bold"
+        >
           Flash Memo
         </Link>
       </div>
