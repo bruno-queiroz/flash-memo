c5e7a991c9d07fe64482c6fd0a12811171a511ac
bruno <brunoqueiroz612@gmail.com>
2023-05-01 16:23:22 -0300

    Create SignIn page

---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Form from "../components/Form";
+import Input from "../components/Input";
+import { Link } from "react-router-dom";
+
+const SignIn = () => {
+  return (
+    <section className="flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold my-8 dark:text-aqua-blue text-dark-blue text-center">
+        Sign Up
+      </h1>
+
+      <Form>
+        <Input labelText="Your Name" />
+        <Input labelText="Your Password" />
+        <button className="bg-primary-yellow py-2 px-4 rounded w-[max-content] mx-auto mt-4 text-white">
+          Sign In
+        </button>
+        <Link to="/sign-up" className="text-center dark:text-gray-300 ">
+          Don't have an account? click here
+        </Link>
+      </Form>
+    </section>
+  );
+};
+
+export default SignIn;
