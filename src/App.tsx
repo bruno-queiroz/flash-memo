a249b8bb7fd988e278a01c43e869391d7ead5818
bruno <brunoqueiroz612@gmail.com>
2023-05-01 08:20:34 -0300

    Change theme depending on prefers-color-scheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    const isDarkThemePreferred = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    if (isDarkThemePreferred) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
   return (
     <>
       <Header />
