362019d62b183e4f437d885984cb1c7c386c130e
bruno <brunoqueiroz612@gmail.com>
2023-05-01 16:23:35 -0300

    Add sign-in route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Header from "./components/Header";
 import { useEffect } from "react";
 import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 const App = () => {
   useEffect(() => {
@@ -21,6 +22,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
       </Routes>
     </>
   );
