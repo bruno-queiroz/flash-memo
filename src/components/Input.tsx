b3811b18d40418f355dfec57c690dd035c0d482d
bruno <brunoqueiroz612@gmail.com>
2023-05-05 07:25:46 -0300

    Add forwardRef to Input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,13 +4,19 @@ type InputProps = {
   labelText: string;
 } & InputHTMLAttributes<HTMLInputElement>;
 
-const Input = ({ labelText, ...rest }: InputProps) => {
-  return (
-    <label className="flex flex-col gap-2">
-      <span className="dark:text-gray-300 text-black">{labelText}</span>
-      <input {...rest} className="dark:bg-neutral-800 p-2 dark:text-gray-300" />
-    </label>
-  );
-};
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ labelText, ...rest }, ref) => {
+    return (
+      <label className="flex flex-col gap-2">
+        <span className="dark:text-gray-300 text-black">{labelText}</span>
+        <input
+          {...rest}
+          ref={ref}
+          className="dark:bg-neutral-800 p-2 dark:text-gray-300"
+        />
+      </label>
+    );
+  }
+);
 
 export default Input;
