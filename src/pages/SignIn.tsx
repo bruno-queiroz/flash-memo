c642115d1cd2df275d2c07e2b954f98d708d3684
bruno <brunoqueiroz612@gmail.com>
2023-05-08 08:34:31 -0300

    Add notifications when signing up and a loader

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useRef } from "react";
 import Form from "../components/Form";
 import Input from "../components/Input";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useMutation } from "react-query";
 import { postSignIn } from "../fetch/postSignIn";
 import { UserForm } from "./SignUp";
+import Notification from "../components/Notification";
+import LoadSpinner from "../components/LoadSpinner";
 
 const SignIn = () => {
   const nameInputRef = useRef<HTMLInputElement>(null);
   const passwordInputRef = useRef<HTMLInputElement>(null);
-  const { mutate: userSignInMutate, data } = useMutation((user: UserForm) =>
-    postSignIn(user)
-  );
+  const {
+    mutate: userSignInMutate,
+    data,
+    isLoading,
+  } = useMutation((user: UserForm) => postSignIn(user));
 
   const signIn = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -25,9 +29,6 @@ const SignIn = () => {
     }
   };
 
-  useEffect(() => {
-    console.log({ signIn: data });
-  }, [data]);
   return (
     <section className="flex flex-col items-center justify-center p-4">
       <h1 className="text-4xl font-bold my-8 dark:text-aqua-blue text-dark-blue text-center">
@@ -37,9 +38,10 @@ const SignIn = () => {
       <Form onSubmit={signIn}>
         <Input labelText="Your Name" ref={nameInputRef} />
         <Input labelText="Your Password" ref={passwordInputRef} />
-        <button className="bg-primary-yellow py-2 px-4 rounded w-[max-content] mx-auto mt-4 text-white">
-          Sign In
+        <button className="bg-primary-yellow py-2 px-4 min-w-[80px] rounded w-[max-content] mx-auto mt-4 text-white">
+          {isLoading ? <LoadSpinner /> : "Sign In"}
         </button>
+        <Notification {...data} />
         <Link to="/sign-up" className="text-center dark:text-gray-300 ">
           Don't have an account? click here
         </Link>
