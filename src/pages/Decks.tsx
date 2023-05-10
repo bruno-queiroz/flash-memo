c401ecc61fca38e3e0a1fea0ba40e48d8b72a18a
bruno <brunoqueiroz612@gmail.com>
2023-05-10 07:45:55 -0300

    Add CreateDeckModal

--- src/pages/Decks.tsx
+++ src/pages/Decks.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import { useState } from "react";
 import DeckStatus from "../components/DeckStatus";
+import CreateDeckModal from "../components/CreateDeckModal";
 
 const Decks = () => {
+  const [isCreateDeckModalOpen, setIsCreateDeckModalOpen] = useState(false);
+
   return (
     <section className="flex flex-col gap-6 items-center p-4 dark:text-gray-300">
+      <CreateDeckModal
+        {...{
+          isModalOpen: isCreateDeckModalOpen,
+          setIsModalOpen: setIsCreateDeckModalOpen,
+        }}
+      />
       <h1 className="font-bold text-center text-4xl text-dark-blue dark:text-aqua-blue">
         Decks
       </h1>
@@ -21,6 +30,7 @@ const Decks = () => {
       <button
         type="button"
         className="bg-primary-yellow py-2 px-4 rounded w-[max-content] mx-auto mt-4 text-white"
+        onClick={() => setIsCreateDeckModalOpen(true)}
       >
         Create Deck
       </button>
