0e12319b542f02ee38459e488e9d3c61175da00f
bruno <brunoqueiroz612@gmail.com>
2023-05-10 07:40:33 -0300

    Add second variant color to Input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,17 +2,22 @@ import React, { InputHTMLAttributes } from "react";
 
 type InputProps = {
   labelText: string;
+  isSecondVariant?: boolean;
 } & InputHTMLAttributes<HTMLInputElement>;
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ labelText, ...rest }, ref) => {
+  ({ labelText, isSecondVariant, ...rest }, ref) => {
     return (
       <label className="flex flex-col gap-2">
         <span className="dark:text-gray-300 text-black">{labelText}</span>
         <input
           {...rest}
           ref={ref}
-          className="dark:bg-neutral-800 p-2 dark:text-gray-300"
+          className={`${
+            isSecondVariant
+              ? "dark:bg-neutral-700 bg-gray-400"
+              : "dark:bg-neutral-800"
+          } p-2 dark:text-gray-300`}
         />
       </label>
     );
