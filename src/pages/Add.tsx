4cc5d3311b7d6636331b0035af95fda6a0524db4
bruno <brunoqueiroz612@gmail.com>
2023-05-11 08:32:19 -0300

    Fetch decks and display as select options

--- src/pages/Add.tsx
+++ src/pages/Add.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import { useQuery } from "react-query";
 import Form from "../components/Form";
+import { getDecks } from "../fetch/getDecks";
 
 const Add = () => {
+  const { data: decks } = useQuery("decksData", getDecks);
+
   return (
-    <section className="flex gap-4 flex-col items-center dark:text-gray-300 text-black">
+    <section className="flex gap-4 flex-col p-4 items-center dark:text-gray-300 text-black">
       <h1 className="font-bold text-center text-4xl text-dark-blue dark:text-aqua-blue">
         Add
       </h1>
@@ -12,7 +15,9 @@ const Add = () => {
         <label className="flex flex-col gap-2">
           <span>Deck</span>
           <select className="dark:bg-neutral-800 bg-white p-1">
-            <option value="test">Test</option>
+            {decks?.data?.deck?.map((deck) => (
+              <option value={deck?.id}>{deck?.name}</option>
+            ))}
             <option value="test2">Test2</option>
           </select>
         </label>
