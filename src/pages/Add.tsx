2f95aec623dd2378f8d68515a3cdb5d9981b3d15
bruno <brunoqueiroz612@gmail.com>
2023-05-11 15:43:13 -0300

    Submit the card

--- src/pages/Add.tsx
+++ src/pages/Add.tsx
@@ -1,9 +1,31 @@
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import Form from "../components/Form";
 import { getDecks } from "../fetch/getDecks";
+import { useRef } from "react";
+import { CardForm, createCard } from "../fetch/createCard";
 
 const Add = () => {
   const { data: decks } = useQuery("decksData", getDecks);
+  const { mutate: cardMutate } = useMutation((newCard: CardForm) =>
+    createCard(newCard)
+  );
+  const cardSelectRef = useRef<HTMLSelectElement>(null);
+  const cardFrontInputRef = useRef<HTMLTextAreaElement>(null);
+  const cardBackInputRef = useRef<HTMLTextAreaElement>(null);
+
+  const submitCard = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const newCard = {
+      deckId: cardSelectRef.current?.value,
+      front: cardFrontInputRef.current?.value,
+      back: cardBackInputRef.current?.value,
+    };
+
+    if (newCard?.back && newCard?.front && newCard?.deckId) {
+      cardMutate(newCard as CardForm);
+    }
+  };
 
   return (
     <section className="flex gap-4 flex-col p-4 items-center dark:text-gray-300 text-black">
@@ -11,20 +33,24 @@ const Add = () => {
         Add
       </h1>
 
-      <Form>
+      <Form onSubmit={submitCard}>
         <label className="flex flex-col gap-2">
           <span>Deck</span>
-          <select required className="dark:bg-neutral-800 bg-white p-1">
+          <select
+            required
+            className="dark:bg-neutral-800 bg-white p-1"
+            ref={cardSelectRef}
+          >
             {decks?.data?.deck?.map((deck) => (
               <option value={deck?.id}>{deck?.name}</option>
             ))}
-            <option value="test2">Test2</option>
           </select>
         </label>
         <label className="flex flex-col gap-2">
           <span>Card Front</span>
           <textarea
             required
+            ref={cardFrontInputRef}
             className="dark:bg-neutral-800 bg-white p-2"
           ></textarea>
         </label>
@@ -32,6 +58,7 @@ const Add = () => {
           <span>Card Back</span>
           <textarea
             required
+            ref={cardBackInputRef}
             className="dark:bg-neutral-800 bg-white p-2"
           ></textarea>
         </label>
