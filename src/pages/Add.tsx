289e6e14dfffe26482c963d270535bcbe753fc27
bruno <brunoqueiroz612@gmail.com>
2023-05-11 08:42:37 -0300

    Turn form fields required

--- src/pages/Add.tsx
+++ src/pages/Add.tsx
@@ -14,7 +14,7 @@ const Add = () => {
       <Form>
         <label className="flex flex-col gap-2">
           <span>Deck</span>
-          <select className="dark:bg-neutral-800 bg-white p-1">
+          <select required className="dark:bg-neutral-800 bg-white p-1">
             {decks?.data?.deck?.map((deck) => (
               <option value={deck?.id}>{deck?.name}</option>
             ))}
@@ -23,11 +23,17 @@ const Add = () => {
         </label>
         <label className="flex flex-col gap-2">
           <span>Card Front</span>
-          <textarea className="dark:bg-neutral-800 bg-white p-2"></textarea>
+          <textarea
+            required
+            className="dark:bg-neutral-800 bg-white p-2"
+          ></textarea>
         </label>
         <label className="flex flex-col gap-2">
           <span>Card Back</span>
-          <textarea className="dark:bg-neutral-800 bg-white p-2"></textarea>
+          <textarea
+            required
+            className="dark:bg-neutral-800 bg-white p-2"
+          ></textarea>
         </label>
         <button className="bg-primary-yellow py-2 px-4 rounded w-[max-content] mx-auto mt-4 text-white">
           Create
