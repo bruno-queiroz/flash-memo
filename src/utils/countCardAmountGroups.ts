48b9184608187a46364d20041a91ccc471964d3e
bruno <brunoqueiroz612@gmail.com>
2023-05-16 16:11:40 -0300

    Create countCardAmountGroups utility function

---
+++ src/utils/countCardAmountGroups.ts
@@ -0,0 +1,31 @@
+import { Card } from "../fetch/getStudyDeck";
+
+export interface CardAmountGroups {
+  resetedCards: number;
+  newCards: number;
+  reviewCards: number;
+}
+
+export const countCardAmountGroups = (cards: Card[] | undefined) => {
+  let resetedCards = 0;
+  let newCards = 0;
+  let reviewCards = 0;
+
+  if (cards) {
+    cards.forEach((card) => {
+      if (card.wasCardReseted && card.reviewAwaitTime === 0) {
+        resetedCards++;
+      } else if (!card.wasCardReseted && card.reviewAwaitTime === 0) {
+        newCards++;
+      } else {
+        reviewCards++;
+      }
+    });
+  }
+
+  return {
+    resetedCards,
+    newCards,
+    reviewCards,
+  };
+};
