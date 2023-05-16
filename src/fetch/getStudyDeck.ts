a71b01b9a53355a54c8eef35cfcb7b20f980f090
bruno <brunoqueiroz612@gmail.com>
2023-05-16 16:10:28 -0300

    Add return types to getStudyDeck

--- src/fetch/getStudyDeck.ts
+++ src/fetch/getStudyDeck.ts
@@ -1,5 +1,25 @@
+import { ServerResponse } from "./postSignIn";
+
+export interface Card {
+  front: string;
+  back: string;
+  createdAt: Date;
+  id: string;
+  reviewAt: Date | null;
+  reviewAwaitTime: number;
+  wasCardReseted: boolean;
+}
+
+export interface DeckStudy {
+  createdAt: Date;
+  id: string;
+  name: string;
+  userId: string;
+  cards: Card[];
+}
+
 export const getStudyDeck = async (deckName: string | undefined) => {
   const response = await fetch(`http://localhost:3000/study-deck/${deckName}`);
-  const data = await response.json();
+  const data: ServerResponse<DeckStudy> = await response.json();
   return data;
 };
