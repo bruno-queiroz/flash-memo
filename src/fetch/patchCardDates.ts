a415eafc4f54b9d00a409aad6511f3f58f7cd2c1
bruno <brunoqueiroz612@gmail.com>
2023-05-23 20:35:05 -0300

    Change patchCardDates params and cardDates

--- src/fetch/patchCardDates.ts
+++ src/fetch/patchCardDates.ts
@@ -1,12 +1,21 @@
 import { Card } from "./getStudyDeck";
+import {
+  RecallFeedback,
+  multiplyReviewTime,
+} from "../utils/multiplyReviewTime";
 
-export const patchCardDates = async (card: Card) => {
+export const patchCardDates = async (
+  card: Card,
+  recallFeedback: keyof RecallFeedback
+) => {
+  card.wasCardReseted;
   const cardDates = {
-    reviewAt: card.reviewAt,
-    reviewAwaitTime: card.reviewAwaitTime,
+    reviewAt: card?.reviewAt,
+    reviewAwaitTime: multiplyReviewTime(card?.reviewAwaitTime, recallFeedback),
+    wasCardReseted: recallFeedback === "reset",
   };
   const response = await fetch(
-    `http://localhost:3000/patch-card-dates/${card.id}`,
+    `http://localhost:3000/patch-card-dates/${card?.id}`,
     {
       method: "PATCH",
       headers: {
