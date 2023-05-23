04036ccc46856a72ca4f63b0d42ee58d988bff67
bruno <brunoqueiroz612@gmail.com>
2023-05-23 20:36:56 -0300

    Create getSearchedCards fetch function

---
+++ src/fetch/getSearchedCards.ts
@@ -0,0 +1,27 @@
+import { DeckStudy } from "./getStudyDeck";
+import { ServerResponse } from "./postSignIn";
+
+export const getSearchedCards = async (
+  deckId: string | undefined,
+  query: string | undefined
+) => {
+  if (!deckId || !query) {
+    return;
+  }
+  try {
+    const response = await fetch(
+      `http://localhost:3000/search-cards/${deckId}/${query}`,
+      {
+        credentials: "include",
+      }
+    );
+    const data: ServerResponse<DeckStudy> = await response.json();
+
+    if (data?.data.cards.length === 0) {
+      throw new Error("Card not found");
+    }
+    return data;
+  } catch (err) {
+    throw new Error("test");
+  }
+};
