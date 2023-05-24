3bd90c6e2ae9adb23c94b844925a35a363e2f873
bruno <brunoqueiroz612@gmail.com>
2023-05-24 16:26:33 -0300

    Create CardNotFound component

---
+++ src/components/CardNotFound.tsx
@@ -0,0 +1,9 @@
+const CardNotFound = ({ errorMessage }: { errorMessage: string }) => {
+  return (
+    <div className="grid place-items-center bg-gray-200 dark:bg-neutral-900 rounded p-4">
+      {errorMessage}
+    </div>
+  );
+};
+
+export default CardNotFound;
