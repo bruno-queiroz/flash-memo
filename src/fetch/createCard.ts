afcc1dc2b1f9eba932e6637e1e2321322f1a8691
bruno <brunoqueiroz612@gmail.com>
2023-05-26 16:30:45 -0300

    Add try and catch to createCard fetch function

--- src/fetch/createCard.ts
+++ src/fetch/createCard.ts
@@ -7,15 +7,24 @@ export interface CardForm {
 }
 
 export const createCard = async (newCard: CardForm) => {
-  const response = await fetch(`http://localhost:3000/create-card`, {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify(newCard),
-    credentials: "include",
-  });
+  try {
+    const response = await fetch(`http://localhost:3000/create-card`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newCard),
+      credentials: "include",
+    });
 
-  const data: ServerResponse<null> = await response.json();
-  return data;
+    const data: ServerResponse<null> = await response.json();
+
+    if (!data?.isOk) {
+      throw new Error(data?.msg);
+    }
+    return data;
+  } catch (err) {
+    const errorMsg = (err as Error).message;
+    throw new Error(errorMsg);
+  }
 };
