f4d79695dc837460b996d998d643a8621a50126e
bruno <brunoqueiroz612@gmail.com>
2023-05-26 16:23:34 -0300

    Change CRUDNotification to position fixed

--- src/components/CRUDNotification.tsx
+++ src/components/CRUDNotification.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import { ServerResponse } from "../fetch/postSignIn";
+import { useEffect } from "react";
 import { NotificationContent } from "../pages/Add";
 
 type CRUDNotificationProps = NotificationContent & {
@@ -27,7 +26,7 @@ const CRUDNotification = ({
 
   return (
     <div
-      className={`absolute top-20 left-0 min-w-[150px] p-6 bg-gray-200 shadow-md font-semibold transition-transform dark:bg-neutral-900 rounded-tr rounded-br ${
+      className={`fixed top-20 z-20 left-0 min-w-[150px] p-6 bg-gray-200 shadow-md font-semibold transition-transform dark:bg-neutral-900 rounded-tr rounded-br ${
         isOk ? "text-green-600" : "text-red-500"
       } ${isNotificationShowing ? "translate-x-0" : "translate-x-[-100%]"}`}
     >
