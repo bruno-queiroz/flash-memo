5ea1845083d42f326e3e60fa8b87cff444d30a07
bruno <brunoqueiroz612@gmail.com>
2023-05-26 16:30:04 -0300

    Add EditModal to SearchCard component

--- src/components/SearchCard.tsx
+++ src/components/SearchCard.tsx
@@ -1,13 +1,30 @@
-import React from "react";
 import { Card } from "../fetch/getStudyDeck";
+import { RiDeleteBin7Line as DeleteIcon } from "react-icons/ri";
 import moment from "moment";
+import ModalContainer from "./ModalContainer";
+import { useState } from "react";
+import EditCardModal from "./EditCardModal";
+import { NotificationContentProvider } from "../context/NotificationContext";
 
-const SearchCard = ({ front, back, reviewAt }: Card) => {
+const SearchCard = ({ front, back, reviewAt, id }: Card) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openEditModal = () => {
+    setIsModalOpen(true);
+  };
+
   const ReviewAtNotNull = reviewAt || 1000;
   const isCardAvailable =
     new Date().getTime() < new Date(ReviewAtNotNull).getTime();
+
   return (
     <article className="bg-gray-200 dark:bg-neutral-900 rounded px-4 pb-4">
+      <NotificationContentProvider>
+        <ModalContainer {...{ isModalOpen, setIsModalOpen }}>
+          <EditCardModal cardId={id} />
+        </ModalContainer>
+      </NotificationContentProvider>
+
       <div className="py-4">
         <h2>{front}</h2>
       </div>
@@ -16,9 +33,17 @@ const SearchCard = ({ front, back, reviewAt }: Card) => {
         <p>{back}</p>
       </div>
       <div className="flex justify-between items-center">
-        <button className="bg-primary-yellow py-1 px-3 rounded font-semibold text-neutral-900">
-          Edit
-        </button>
+        <div className="flex gap-4">
+          <button
+            className="bg-primary-yellow py-1 px-3 rounded font-semibold text-neutral-900"
+            onClick={openEditModal}
+          >
+            Edit
+          </button>
+          <button className="bg-gray-300 dark:bg-neutral-800 rounded py-1 px-3">
+            <DeleteIcon />
+          </button>
+        </div>
         <div>
           <span>Review in:</span>{" "}
           <span>
