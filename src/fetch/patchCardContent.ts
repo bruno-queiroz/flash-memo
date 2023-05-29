b97efd036c36a31b85a91fddf74d4f88834f3da6
bruno <brunoqueiroz612@gmail.com>
2023-05-29 16:54:40 -0300

    Create patchCardContent fetch function

---
+++ src/fetch/patchCardContent.ts
@@ -0,0 +1,31 @@
+import { Card } from "./getStudyDeck";
+import { ServerResponse } from "./postSignIn";
+
+export const patchCardContent = async (
+  newCard: Pick<Card, "front" | "back">,
+  cardId: string
+) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/patch-card-content/${cardId}`,
+      {
+        method: "PATCH",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(newCard),
+        credentials: "include",
+      }
+    );
+    const data: ServerResponse<null> = await response.json();
+    if (!data?.isOk) {
+      throw new Error(data?.msg);
+    }
+
+    return data;
+  } catch (err) {
+    const errorMsg = (err as Error).message;
+
+    throw new Error(errorMsg);
+  }
+};
