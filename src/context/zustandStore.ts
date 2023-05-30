ea786368bdc3e3c894e621408ba3343fb3817a13
bruno <brunoqueiroz612@gmail.com>
2023-05-30 16:30:46 -0300

    Add cardEditData state

--- src/context/zustandStore.ts
+++ src/context/zustandStore.ts
@@ -1,16 +1,23 @@
 import { create } from "zustand";
+import { Card } from "../fetch/getStudyDeck";
+
+type CardEditData = Pick<Card, "back" | "front" | "id">;
 
 interface FlashMemoState {
   isCreateDeckModalOpen: boolean;
-  setIsCreateDeckModalOpen: (boolean: boolean) => void;
   isEditModalOpen: boolean;
+  cardEditData: CardEditData;
+  setIsCreateDeckModalOpen: (boolean: boolean) => void;
   setIsEditModalOpen: (boolean: boolean) => void;
+  setCardEditData: (cardEditData: CardEditData) => void;
 }
 
 export const useFlashMemoStore = create<FlashMemoState>()((set) => ({
   isCreateDeckModalOpen: false,
   isEditModalOpen: false,
+  cardEditData: {} as CardEditData,
   setIsCreateDeckModalOpen: (boolean) =>
     set(() => ({ isCreateDeckModalOpen: boolean })),
   setIsEditModalOpen: (boolean) => set(() => ({ isEditModalOpen: boolean })),
+  setCardEditData: (cardEditData) => set(() => ({ cardEditData })),
 }));
