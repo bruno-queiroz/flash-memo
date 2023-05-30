024cf6d9edb0b8748567d86f7979cab600f17fa9
bruno <brunoqueiroz612@gmail.com>
2023-05-30 16:24:18 -0300

    Add states to EditModal

--- src/context/zustandStore.ts
+++ src/context/zustandStore.ts
@@ -3,10 +3,14 @@ import { create } from "zustand";
 interface FlashMemoState {
   isCreateDeckModalOpen: boolean;
   setIsCreateDeckModalOpen: (boolean: boolean) => void;
+  isEditModalOpen: boolean;
+  setIsEditModalOpen: (boolean: boolean) => void;
 }
 
 export const useFlashMemoStore = create<FlashMemoState>()((set) => ({
   isCreateDeckModalOpen: false,
+  isEditModalOpen: false,
   setIsCreateDeckModalOpen: (boolean) =>
     set(() => ({ isCreateDeckModalOpen: boolean })),
+  setIsEditModalOpen: (boolean) => set(() => ({ isEditModalOpen: boolean })),
 }));
