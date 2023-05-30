18d31f66a4469ecbb9fcc219b859f54199bf211d
bruno <brunoqueiroz612@gmail.com>
2023-05-30 16:34:16 -0300

    Open EditModal when active

--- src/components/EditCardModal.tsx
+++ src/components/EditCardModal.tsx
@@ -1,14 +1,19 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import Form from "./Form";
 import LoadSpinner from "./LoadSpinner";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 import { getSingleCard } from "../fetch/getSingleCard";
 import { patchCardContent } from "../fetch/patchCardContent";
 import { Card } from "../fetch/getStudyDeck";
-import { NotificationContext } from "../context/NotificationContext";
+import ModalContainer from "./ModalContainer";
+import { useFlashMemoStore } from "../context/zustandStore";
 
 const EditCardModal = ({ cardId }: { cardId: string }) => {
-  const [_, setNotificationContent] = useContext(NotificationContext);
+  const isEditModalOpen = useFlashMemoStore((state) => state.isEditModalOpen);
+  const setIsEditModalOpen = useFlashMemoStore(
+    (state) => state.setIsEditModalOpen
+  );
+
   const cardFrontInputRef = useRef<HTMLTextAreaElement>(null);
   const cardBackInputRef = useRef<HTMLTextAreaElement>(null);
   const queryClient = useQueryClient();
@@ -62,45 +67,39 @@ const EditCardModal = ({ cardId }: { cardId: string }) => {
     }
   }, [isGetSingleCardSuccess]);
 
-  useEffect(() => {
-    if (isEditCardError) {
-      const errorMsg = (editCardError as Error).message;
-
-      setNotificationContent({
-        isNotificationShowing: true,
-        isOk: false,
-        msg: errorMsg,
-      });
-      return;
-    }
-  }, [isEditCardError]);
-
   return (
     <>
-      <Form className="flex flex-col items-center gap-4" onSubmit={editCard}>
-        <h2 className="font-semibold text-center text-2xl text-dark-blue dark:text-white">
-          Edit Card
-        </h2>
-        <label className="flex flex-col gap-2 w-full">
-          <span>Card Front</span>
-          <textarea
-            required
-            ref={cardFrontInputRef}
-            className="dark:bg-neutral-900 bg-white p-2"
-          />
-        </label>
-        <label className="flex flex-col gap-2 w-full">
-          <span>Card Back</span>
-          <textarea
-            required
-            ref={cardBackInputRef}
-            className="dark:bg-neutral-900 bg-white p-2"
-          />
-        </label>
-        <button className="bg-primary-yellow py-2 px-4 rounded w-[80px] mx-auto mt-4 text-white">
-          {isEditCardLoading ? <LoadSpinner /> : "Edit"}
-        </button>
-      </Form>
+      <ModalContainer
+        {...{
+          isModalOpen: isEditModalOpen,
+          setIsModalOpen: setIsEditModalOpen,
+        }}
+      >
+        <Form className="flex flex-col items-center gap-4" onSubmit={editCard}>
+          <h2 className="font-semibold text-center text-2xl text-dark-blue dark:text-white">
+            Edit Card
+          </h2>
+          <label className="flex flex-col gap-2 w-full">
+            <span>Card Front</span>
+            <textarea
+              required
+              ref={cardFrontInputRef}
+              className="dark:bg-neutral-900 bg-white p-2"
+            />
+          </label>
+          <label className="flex flex-col gap-2 w-full">
+            <span>Card Back</span>
+            <textarea
+              required
+              ref={cardBackInputRef}
+              className="dark:bg-neutral-900 bg-white p-2"
+            />
+          </label>
+          <button className="bg-primary-yellow py-2 px-4 rounded w-[80px] mx-auto mt-4 text-white">
+            {isEditCardLoading ? <LoadSpinner /> : "Edit"}
+          </button>
+        </Form>
+      </ModalContainer>
     </>
   );
 };
