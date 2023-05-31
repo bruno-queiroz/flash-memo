4d6d9ed6a7856808cc6508190f23ce3754566e71
bruno <brunoqueiroz612@gmail.com>
2023-05-31 16:35:32 -0300

    Remove LoadSpinner bg color

--- src/components/LoadSpinner.tsx
+++ src/components/LoadSpinner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LoadSpinner = () => {
   return (
-    <div className="w-[20px] mx-auto h-[20px] rounded-[50%] border-t-white border-t-[4px] bg-primary-yellow border-l-white border-l-[4px] border-transparent border-[4px] animate-spin" />
+    <div className="w-[20px] mx-auto h-[20px] rounded-[50%] border-t-white border-t-[4px] bg-transparent border-l-white border-l-[4px] border-transparent border-[4px] animate-spin" />
   );
 };
 
