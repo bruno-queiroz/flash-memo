f2a70e47673227fa5639e60a1e18d805d8c35c5f
bruno <brunoqueiroz612@gmail.com>
2023-05-31 07:35:58 -0300

    Remove react contexts

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,23 +1,15 @@
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import SearchCard from "../components/SearchCard";
 import { getSearchedCards } from "../fetch/getSearchedCards";
 import { useQuery } from "react-query";
 import { getDecks } from "../fetch/getDecks";
 import CardNotFound from "../components/CardNotFound";
-import { ModalContainerProvider } from "../context/ModalContainerContext";
-import { NotificationContentProvider } from "../context/NotificationContext";
-import ModalContainer from "../components/ModalContainer";
 import EditCardModal from "../components/EditCardModal";
-import { Card } from "../fetch/getStudyDeck";
-
-type CardEditData = Pick<Card, "back" | "front" | "id">;
+import CRUDNotification from "../components/CRUDNotification";
 
 const Search = () => {
   const deckSelectRef = useRef<HTMLSelectElement>(null);
   const searchInputRef = useRef<HTMLInputElement>(null);
-  const [cardEditData, setCardEditData] = useState<CardEditData>(
-    {} as CardEditData
-  );
 
   const { data, isLoading, refetch, isError, error } = useQuery(
     ["searchCards"],
@@ -41,64 +33,59 @@ const Search = () => {
 
   return (
     <>
-      <ModalContainerProvider>
-        <NotificationContentProvider>
-          <ModalContainer>
-            <EditCardModal {...cardEditData} />
-          </ModalContainer>
-        </NotificationContentProvider>
+      <CRUDNotification />
+      <EditCardModal />
 
-        <section className="flex flex-col items-center p-4 dark:text-gray-300 text-black">
-          <h1 className="font-bold text-center text-4xl text-dark-blue dark:text-aqua-blue">
-            Search a Card
-          </h1>
-          <div className="max-w-[900px] w-full">
-            <form
-              className="flex flex-col gap-4 items-center mx-auto mt-8"
-              onSubmit={onSearch}
-            >
-              <label className="flex items-center gap-3">
-                <span>Select a deck</span>
-                <select
-                  className="bg-gray-200 dark:bg-neutral-900 p-1"
-                  ref={deckSelectRef}
-                >
-                  {decks?.data?.map((deck) => (
-                    <option value={deck?.id} key={deck?.id}>
-                      {deck?.name}
-                    </option>
-                  ))}
-                </select>
-              </label>
-              <div className="flex gap-2 w-full">
-                <input
-                  type="text"
-                  aria-label="search card"
-                  autoFocus
-                  className="bg-gray-200 dark:bg-neutral-900 w-full p-2"
-                  ref={searchInputRef}
-                  required
-                />
-                <button className="bg-dark-blue dark:bg-aqua-blue dark:text-neutral-900 font-semibold text-white p-2 rounded-tr">
-                  Search
-                </button>
-              </div>
-            </form>
-
-            <div className="flex flex-col gap-4 mt-8">
-              {isLoading ? (
-                "Loading..."
-              ) : isError ? (
-                <CardNotFound errorMessage={(error as Error).message} />
-              ) : (
-                data?.data?.cards?.map((card) => (
-                  <SearchCard {...card} set={setCardEditData} key={card?.id} />
-                ))
-              )}
+      <section className="flex flex-col items-center p-4 dark:text-gray-300 text-black">
+        <h1 className="font-bold text-center text-4xl text-dark-blue dark:text-aqua-blue">
+          Search a Card
+        </h1>
+        <div className="max-w-[900px] w-full">
+          <form
+            className="flex flex-col gap-4 items-center mx-auto mt-8"
+            onSubmit={onSearch}
+          >
+            <label className="flex items-center gap-3">
+              <span>Select a deck</span>
+              <select
+                className="bg-gray-200 dark:bg-neutral-900 p-1"
+                ref={deckSelectRef}
+              >
+                {decks?.data?.map((deck) => (
+                  <option value={deck?.id} key={deck?.id}>
+                    {deck?.name}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <div className="flex gap-2 w-full">
+              <input
+                type="text"
+                aria-label="search card"
+                autoFocus
+                className="bg-gray-200 dark:bg-neutral-900 w-full p-2"
+                ref={searchInputRef}
+                required
+              />
+              <button className="bg-dark-blue dark:bg-aqua-blue dark:text-neutral-900 font-semibold text-white p-2 rounded-tr">
+                Search
+              </button>
             </div>
+          </form>
+
+          <div className="flex flex-col gap-4 mt-8">
+            {isLoading ? (
+              "Loading..."
+            ) : isError ? (
+              <CardNotFound errorMessage={(error as Error).message} />
+            ) : (
+              data?.data?.cards?.map((card) => (
+                <SearchCard {...card} key={card?.id} />
+              ))
+            )}
           </div>
-        </section>
-      </ModalContainerProvider>
+        </div>
+      </section>
     </>
   );
 };
