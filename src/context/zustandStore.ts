719ce9885bfdaf94cb4211d0512f09c43c118c13
bruno <brunoqueiroz612@gmail.com>
2023-05-31 07:11:51 -0300

    Add notificationContent state

--- src/context/zustandStore.ts
+++ src/context/zustandStore.ts
@@ -3,21 +3,32 @@ import { Card } from "../fetch/getStudyDeck";
 
 type CardEditData = Pick<Card, "back" | "front" | "id">;
 
+interface NotificationContent {
+  isOk: boolean | undefined;
+  msg: string | undefined;
+  isNotificationShowing: boolean;
+}
+
 interface FlashMemoState {
   isCreateDeckModalOpen: boolean;
   isEditModalOpen: boolean;
   cardEditData: CardEditData;
+  notificationContent: NotificationContent;
   setIsCreateDeckModalOpen: (boolean: boolean) => void;
   setIsEditModalOpen: (boolean: boolean) => void;
   setCardEditData: (cardEditData: CardEditData) => void;
+  setNotificationContent: (content: NotificationContent) => void;
 }
 
 export const useFlashMemoStore = create<FlashMemoState>()((set) => ({
   isCreateDeckModalOpen: false,
   isEditModalOpen: false,
   cardEditData: {} as CardEditData,
+  notificationContent: {} as NotificationContent,
   setIsCreateDeckModalOpen: (boolean) =>
     set(() => ({ isCreateDeckModalOpen: boolean })),
   setIsEditModalOpen: (boolean) => set(() => ({ isEditModalOpen: boolean })),
   setCardEditData: (cardEditData) => set(() => ({ cardEditData })),
+  setNotificationContent: (notificationContent) =>
+    set(() => ({ notificationContent })),
 }));
