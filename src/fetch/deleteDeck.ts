8122b6f6ab746bc3a7a085b762224ba4b7f1f898
bruno <brunoqueiroz612@gmail.com>
2023-05-31 16:35:48 -0300

    Create deleteDeck fetch function

---
+++ src/fetch/deleteDeck.ts
@@ -0,0 +1,24 @@
+import { ServerResponse } from "./postSignIn";
+
+export const deleteDeck = async (deckId: string) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/delete-deck/${deckId}`,
+      {
+        method: "DELETE",
+        credentials: "include",
+      }
+    );
+
+    const data: ServerResponse<null> = await response.json();
+
+    if (!data?.isOk) {
+      throw new Error(data?.msg);
+    }
+
+    return data;
+  } catch (err) {
+    const errorMessage = (err as Error).message;
+    throw new Error(errorMessage);
+  }
+};
