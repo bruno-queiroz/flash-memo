c80264691b121af4032649bf653be14ed6106a0a
bruno <brunoqueiroz612@gmail.com>
2023-06-05 07:34:47 -0300

    Change mutate to mutateAsync

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Form from "../components/Form";
 import Input from "../components/Input";
 import { useMutation } from "react-query";
-import { postUser } from "../fetch/postUser";
-import LoginNotification from "../components/LoginNotification";
+import { postSignUp } from "../fetch/postSignUp";
+import LoginErrorMessage from "../components/LoginErrorMessage";
 import LoadSpinner from "../components/LoadSpinner";
+import { useFlashMemoStore } from "../context/zustandStore";
 
 export interface UserForm {
   name: string;
@@ -17,12 +18,14 @@ const SignUp = () => {
   const passwordInputRef = useRef<HTMLInputElement>(null);
   const navigate = useNavigate();
   const {
-    mutate: newUserMutation,
+    mutateAsync: newUserMutation,
     isLoading,
-    data,
-  } = useMutation((user: UserForm) => postUser(user));
+    error,
+  } = useMutation((user: UserForm) => postSignUp(user));
 
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const setIsUserLogged = useFlashMemoStore((state) => state.setIsUserLogged);
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (nameInputRef.current && passwordInputRef.current) {
@@ -30,15 +33,17 @@ const SignUp = () => {
         name: nameInputRef.current?.value,
         password: passwordInputRef.current?.value,
       };
-      newUserMutation(newUser);
+
+      try {
+        await newUserMutation(newUser);
+
+        setIsUserLogged(true);
+        navigate("/decks");
+      } catch (err) {
+        console.log(err);
+      }
     }
   };
-
-  useEffect(() => {
-    if (data?.isOk) {
-      navigate("/decks");
-    }
-  }, [data]);
 
   return (
     <section className="flex flex-col items-center justify-center p-4">
@@ -46,7 +51,13 @@ const SignUp = () => {
         Sign Up
       </h1>
       <Form onSubmit={onSubmit}>
-        <Input labelText="Your Name" required type="text" ref={nameInputRef} />
+        <Input
+          labelText="Your Name"
+          required
+          type="text"
+          ref={nameInputRef}
+          autoFocus
+        />
         <Input
           labelText="Create your password"
           required
@@ -57,7 +68,7 @@ const SignUp = () => {
         <button className="bg-primary-yellow py-2 px-4 rounded w-[max-content] mx-auto mt-4 text-white">
           {isLoading ? <LoadSpinner /> : "Sign Up"}
         </button>
-        <LoginNotification {...data} />
+        <LoginErrorMessage errorMessage={(error as Error)?.message} />
         <Link to="/sign-in" className="text-center dark:text-gray-300 ">
           Already have an account? click here
         </Link>
