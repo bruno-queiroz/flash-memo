09619bee77c94fb2458dfdd7c37692774081fd66
bruno <brunoqueiroz612@gmail.com>
2023-06-05 07:33:40 -0300

    Add try and catch to postSignUp fetch function

--- src/fetch/postSignUp.ts
+++ src/fetch/postSignUp.ts
@@ -2,15 +2,25 @@ import { UserForm } from "../pages/SignUp";
 import { ServerResponse } from "./postSignIn";
 
 export const postSignUp = async (user: UserForm) => {
-  const response = await fetch(`http://localhost:3000/sign-up`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(user),
-    credentials: "include",
-  });
+  try {
+    const response = await fetch(`http://localhost:3000/sign-up`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+      credentials: "include",
+    });
 
-  const data: ServerResponse<UserForm> = await response.json();
-  return data;
+    const data: ServerResponse<UserForm> = await response.json();
+
+    if (!data?.isOk) {
+      throw new Error(data?.msg);
+    }
+
+    return data;
+  } catch (err) {
+    const errorMessage = (err as Error)?.message;
+    throw new Error(errorMessage);
+  }
 };
