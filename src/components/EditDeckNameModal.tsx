57cc4728663d707ae3673c07fa2c0b3ff0c01df8
bruno <brunoqueiroz612@gmail.com>
2023-06-06 16:37:50 -0300

    Change deck name on database

--- src/components/EditDeckNameModal.tsx
+++ src/components/EditDeckNameModal.tsx
@@ -1,14 +1,73 @@
 import ModalContainer from "./ModalContainer";
 import { useFlashMemoStore } from "../context/zustandStore";
 import Input from "./Input";
+import { useEffect, useRef } from "react";
+import { useMutation, useQueryClient } from "react-query";
+import { patchRenameDeck } from "../fetch/patchRenameDeck";
 
 const EditDeckNameModal = () => {
+  const deckNameInputRef = useRef<HTMLInputElement>(null);
+  const queryClient = useQueryClient();
+  const { mutateAsync: renameDeckMutate } = useMutation(
+    ({
+      deckId,
+      deckName,
+      isUserLogged,
+    }: {
+      deckId: string;
+      deckName: string;
+      isUserLogged: boolean;
+    }) => patchRenameDeck(deckId, deckName, isUserLogged),
+    {
+      onSuccess: () => queryClient.invalidateQueries("decksData"),
+    }
+  );
+
+  const { deckName, deckId } = useFlashMemoStore((state) => state.deckData);
+  const isUserLogged = useFlashMemoStore((state) => state.isUserLogged);
   const isEditDeckNameModalOpen = useFlashMemoStore(
     (state) => state.isEditDeckNameModalOpen
   );
   const setIsEditDeckNameModalOpen = useFlashMemoStore(
     (state) => state.setIsEditDeckNameModalOpen
   );
+  const setNotificationContent = useFlashMemoStore(
+    (state) => state.setNotificationContent
+  );
+  const onEditDeckName = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (deckNameInputRef.current) {
+      try {
+        const data = await renameDeckMutate({
+          deckId,
+          deckName: deckNameInputRef.current.value,
+          isUserLogged,
+        });
+
+        setNotificationContent({
+          isNotificationShowing: true,
+          isOk: true,
+          msg: data?.msg,
+        });
+        setIsEditDeckNameModalOpen(false);
+      } catch (err) {
+        const errorMsg = (err as Error).message;
+
+        setNotificationContent({
+          isNotificationShowing: true,
+          isOk: false,
+          msg: errorMsg,
+        });
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (deckNameInputRef.current && deckName) {
+      deckNameInputRef.current.value = deckName;
+    }
+  }, [deckName]);
 
   return (
     <ModalContainer
@@ -17,10 +76,15 @@ const EditDeckNameModal = () => {
         setIsModalOpen: setIsEditDeckNameModalOpen,
       }}
     >
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={onEditDeckName}>
         <h3 className="text-2xl font-semibold">Edit Deck Name</h3>
 
-        <Input labelText="Deck Name" isSecondVariant />
+        <Input
+          labelText="Deck Name"
+          isSecondVariant
+          ref={deckNameInputRef}
+          required
+        />
         <div className="flex justify-end">
           <button className="py-2 px-4 w-[78px] font-semibold rounded dark:bg-neutral-700 bg-gray-200  dark:text-white">
             Edit
