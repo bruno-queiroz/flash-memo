28933a8584ad1cf72d8885bf2499fe7a2abd8272
bruno <brunoqueiroz612@gmail.com>
2023-06-06 08:13:51 -0300

    Add try and catch to postDeck fetch function

--- src/fetch/postDeck.ts
+++ src/fetch/postDeck.ts
@@ -6,21 +6,30 @@ export interface DeckForm {
 }
 
 export const postDeck = async (newDeck: DeckForm, isUserLogged: boolean) => {
-  const response = await fetch(`http://localhost:3000/create-deck`, {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify(newDeck),
-    credentials: "include",
-  });
+  try {
+    const response = await fetch(`http://localhost:3000/create-deck`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newDeck),
+      credentials: "include",
+    });
 
-  const data: ServerResponse<null> = await response.json();
+    const data: ServerResponse<null> = await response.json();
 
-  if (data?.msg === "Session expired") {
-    handleSessionExpired(isUserLogged);
-    return;
+    if (data?.msg === "Session expired") {
+      handleSessionExpired(isUserLogged);
+      return;
+    }
+
+    if (!data?.isOk) {
+      throw new Error(data?.msg);
+    }
+
+    return data;
+  } catch (err) {
+    const errorMessage = (err as Error).message;
+    throw new Error(errorMessage);
   }
-
-  return data;
 };
