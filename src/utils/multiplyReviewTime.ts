0c767232bdf566a6197601d64ea6717d824f3b18
bruno <brunoqueiroz612@gmail.com>
2023-06-12 16:08:37 -0300

    Change the multiply method

--- src/utils/multiplyReviewTime.ts
+++ src/utils/multiplyReviewTime.ts
@@ -1,35 +1,80 @@
-const oneHourInMilliseconds = 3600000;
-const halfHourInMilliseconds = 1800000;
-const tenMinutesInMilliseconds = 600000;
+import {
+  ONE_DAY_IN_MILLISENCONDS,
+  ONE_HOUR_IN_MILLISECONDS,
+  ONE_MONTH_IN_MILLISENCONDS,
+  ONE_YEAR_IN_MILLISECONDS,
+} from "./formatDate";
 
-const recallFeedbackValues = {
-  easy: 1.8,
-  good: 1.5,
-  hard: 1.3,
-  reset: 0,
+export interface RecallFeedback {
+  easy: number;
+  good: number;
+  hard: number;
+  reset: number;
+}
+
+const maxMultiplier = {
+  hard: 2,
+  good: 4,
+  easy: 6,
 };
 
-export type RecallFeedback = typeof recallFeedbackValues;
+const mediumMultiplier = {
+  hard: 1.3,
+  good: 1.5,
+  easy: 1.8,
+};
+
+const minMultiplier = {
+  hard: 0.3,
+  good: 0.4,
+  easy: 0.5,
+};
+
+const moreThanFourMonthsMultiplier = {
+  hard: 3,
+  good: 6,
+  easy: 8,
+};
+
+const moreThanAYearMultiplier = {
+  hard: 4,
+  good: 7,
+  easy: 9,
+};
 
 export const multiplyReviewTime = (
   reviewAwaitTime: number | null | undefined,
-  recallFeedback: keyof RecallFeedback
+  feedback: keyof RecallFeedback
 ) => {
   let reviewTime = 0;
-  if (reviewAwaitTime) {
-    reviewTime = reviewAwaitTime * recallFeedbackValues[recallFeedback];
-    return reviewTime;
+  if (reviewAwaitTime && feedback !== "reset") {
+    if (reviewAwaitTime > ONE_YEAR_IN_MILLISECONDS) {
+      return (
+        ONE_MONTH_IN_MILLISENCONDS * moreThanAYearMultiplier[feedback] +
+        ONE_YEAR_IN_MILLISECONDS
+      );
+    } else if (reviewAwaitTime > ONE_MONTH_IN_MILLISENCONDS * 4) {
+      return (
+        ONE_MONTH_IN_MILLISENCONDS * moreThanFourMonthsMultiplier[feedback]
+      );
+    } else if (reviewAwaitTime > ONE_MONTH_IN_MILLISENCONDS * 3) {
+      return reviewAwaitTime * minMultiplier[feedback];
+    } else if (reviewAwaitTime > ONE_MONTH_IN_MILLISENCONDS) {
+      return reviewAwaitTime * mediumMultiplier[feedback];
+    } else {
+      return reviewAwaitTime * maxMultiplier[feedback];
+    }
   }
 
-  switch (recallFeedback) {
+  switch (feedback) {
     case "hard":
-      reviewTime = tenMinutesInMilliseconds;
+      reviewTime = ONE_HOUR_IN_MILLISECONDS;
       break;
     case "good":
-      reviewTime = halfHourInMilliseconds;
+      reviewTime = ONE_HOUR_IN_MILLISECONDS * 6;
       break;
     case "easy":
-      reviewTime = oneHourInMilliseconds;
+      reviewTime = ONE_DAY_IN_MILLISENCONDS;
       break;
     case "reset":
       reviewTime = 0;
