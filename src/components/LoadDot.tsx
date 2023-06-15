fd68f5a4e30b9a1e902ff96aa7c287906bd13387
bruno <brunoqueiroz612@gmail.com>
2023-06-15 16:19:04 -0300

    Create LoadDot component

---
+++ src/components/LoadDot.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const LoadDot = () => {
+  return (
+    <div>
+      <div className="flex gap-2 justify-center">
+        <div className="w-[25px] h-[25px] rounded-full bg-aqua-blue first-dot"></div>
+        <div className="w-[25px] h-[25px] rounded-full bg-aqua-blue second-dot"></div>
+        <div className="w-[25px] h-[25px] rounded-full bg-aqua-blue third-dot"></div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadDot;
