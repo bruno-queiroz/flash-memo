739b3ffb1509935e4de63f9a8c591bce252ab465
bruno <brunoqueiroz612@gmail.com>
2023-06-16 15:24:50 -0300

    Create CardsCounterSkeleton component

---
+++ src/components/CardsCounterSkeleton.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CardsCounterSkeleton = () => {
+  return (
+    <div className="flex gap-1">
+      <span className="bg-[#05668d] w-[7px] h-[18px]"></span>
+      <span className="bg-[#00a5cf] w-[7px] h-[18px]"></span>
+      <span className="bg-[#25a18e] w-[7px] h-[18px]"></span>
+    </div>
+  );
+};
+
+export default CardsCounterSkeleton;
