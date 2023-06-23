f57414ac09a9ad2a22fbbba2f9f3303a1f3659ad
bruno <brunoqueiroz612@gmail.com>
2023-06-23 08:35:21 -0300

    Create NoDecks component

---
+++ src/components/NoDecks.tsx
@@ -0,0 +1,9 @@
+const NoDecks = () => {
+  return (
+    <div className="dark:bg-neutral-900 shadow-sm bg-gray-200 px-4 py-8 rounded w-[max-content] mx-auto">
+      Looks like you don't have a deck yet. Why don't you create one?{" "}
+    </div>
+  );
+};
+
+export default NoDecks;
