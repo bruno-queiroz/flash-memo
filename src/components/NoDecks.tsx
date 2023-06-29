dacb894140a24013848e9e2c0cacdd84cebe705b
bruno <brunoqueiroz612@gmail.com>
2023-06-29 07:26:58 -0300

    Add responsiveness to NoDecks component

--- src/components/NoDecks.tsx
+++ src/components/NoDecks.tsx
@@ -1,6 +1,6 @@
 const NoDecks = () => {
   return (
-    <div className="dark:bg-neutral-900 shadow-sm bg-gray-200 px-4 py-8 rounded w-[max-content] mx-auto">
+    <div className="dark:bg-neutral-900 shadow-md bg-gray-200 px-4 py-8 rounded w-full text-center max-w-[max-content] mx-auto">
       Looks like you don't have a deck yet. Why don't you create one?{" "}
     </div>
   );
